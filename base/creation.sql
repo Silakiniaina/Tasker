33b8e9786e8dbece02b90aef3bb84df28daf3a2e
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-09 22:57:42 +0300

    Creation table speciality

--- base/creation.sql
+++ base/creation.sql
@@ -1,1 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE specialitiy(
+   id SERIAL,
+   libelle VARCHAR(150)  NOT NULL,
+   description VARCHAR(250),
+   PRIMARY KEY(id),
+   UNIQUE(libelle)
+);
