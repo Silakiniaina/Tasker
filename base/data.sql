3a4d0eaaf65c2208bf09faed838a81ea0dc5d3fb
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-10 14:43:18 +0300

    Mise a jour table speciality et collaborator

--- base/data.sql
+++ base/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO Collaborator(first_name,name,date_of_birth,username,password) VALUES 
-    ('Silakiniaina','Sanda','2005-07-12','Sanda',digest('admin','sha1'));
+INSERT INTO Speciality(libelle,description) VALUES 
+    ('Backend','Concernant les dev back')
+;
+INSERT INTO Collaborator(first_name,name,date_of_birth,username,password,speciality) VALUES 
+    ('Silakiniaina','Sanda','2005-07-12','Sanda',digest('admin','sha1'),1)
+;
