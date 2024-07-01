39662e363ed3c51de36d1fda1a0b3ab4eed2c305
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-01 10:38:10 +0300

    shared : Creation function toSHA256

--- src/shared/Database.java
+++ src/shared/Database.java
@@ -1,5 +1,8 @@
 package shared;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -20,5 +23,26 @@ public class Database {
             throw e ;
         }
         return c;
+    }
+
+    /**
+     * A function to hash a string with the SHA256.
+     * @param str The String to hash with SHA256.
+     * @return The hashed value of the entered parameter.
+     * @throws Exception if the hash doesn't pass.
+     */
+    public static String toSHA256(String str) throws Exception{
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(str.getBytes(StandardCharsets.UTF_8));
+            BigInteger number = new BigInteger(1, hash);
+            StringBuilder hexString = new StringBuilder(number.toString(16));
+            while (hexString.length() < 64) {
+                hexString.insert(0, '0');
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
