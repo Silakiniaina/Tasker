42ef88f69ac2e7d7325a7c25d50755a8f11e1d3b
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-01 09:53:24 +0300

    creation sequences, tables and functions

---
+++ src/script/sequences.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE gender_seq;
+CREATE SEQUENCE project_category_seq;
+CREATE SEQUENCE role_seq;
+CREATE SEQUENCE task_category_seq;
+CREATE SEQUENCE meeting_category_seq;
+CREATE SEQUENCE report_category_seq;
+CREATE SEQUENCE comment_category_seq;
+CREATE SEQUENCE room_seq;
+CREATE SEQUENCE collaborator_seq;
+CREATE SEQUENCE project_seq;
+CREATE SEQUENCE task_seq;
+CREATE SEQUENCE subtask_seq;
+CREATE SEQUENCE meeting_seq;
+CREATE SEQUENCE comment_seq;
+CREATE SEQUENCE report_seq;
+CREATE SEQUENCE team_seq;
