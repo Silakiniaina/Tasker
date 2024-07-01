7f24a025f9c4d03a82298fb770ab2af82bc1084c
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-01 10:35:04 +0300

    shared : Creation function getConnection

--- src/shared/Database.java
+++ src/shared/Database.java
@@ -1,5 +1,24 @@
 package shared;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Database {
-    
+    /**
+     * A function for connecting to the local database.
+     * @return An instance of a Connection if the connection is valid , else return null.
+     * @throws Exception if the connection doesn't pass.
+     */
+    public static Connection getConnection() throws Exception{
+        Connection c = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost/tasker", "sanda", "huhu2005!!");
+        }catch(Exception e){
+            System.out.println("Erreur");
+            e.printStackTrace();
+            throw e ;
+        }
+        return c;
+    }
 }
