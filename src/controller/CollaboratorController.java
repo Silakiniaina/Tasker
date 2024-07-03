82decfbab7c07d338baa9dab0ba3ed57d6661ce6
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-03 16:52:54 +0300

    controller : creation collaborator controller

--- src/controller/CollaboratorController.java
+++ src/controller/CollaboratorController.java
@@ -17,5 +17,48 @@ import shared.Database;
 
 public class CollaboratorController extends HttpServlet{
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mode = request.getParameter("mode");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher disp =  request.getRequestDispatcher("/WEB-INF/views/collaborator/collaborator.jsp");
+        try {
+            if(mode.equals("r")){
+                ArrayList<Collaborator> liste = Collaborator.getAll();
+                request.setAttribute("listeCollaborator", liste);
+                out.println(new Gson().toJson(liste));
+            }else if(mode.equals("d")){
+
+            }
+            //disp.forward(request, response);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mode = request.getParameter("mode");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher disp =  request.getRequestDispatcher("/WEB-INF/views/collaborator/collaborator.jsp");
+        try {
+            String name = request.getParameter("name");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            Date date_of_birth = request.getParameter("dtn") != null ? Date.valueOf(request.getParameter("dtn")) : null;
+            String gender = request.getParameter("gender");
+            String role = request.getParameter("role");
+            Collaborator c = new Collaborator(name, email, gender, role, date_of_birth);
+            c.setPassword(Database.toSHA256(password));
+            if(mode.equals("i")){
+                c.insert();
+                response.sendRedirect("collaborator?mode=r");
+            }else if(mode.equals("u")){
+
+            }
+        } catch (Exception e) {
+            out.println(e.getMessage());
+        }
+    }
     
 }
